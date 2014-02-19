76333582be4f9284d943dc5f1089e99699141185
darealcaffeine <darealcaffeine@gmail.com>
2014-02-19 15:31:28 -0500

    chapter 2 completed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LRE::Application.config.secret_key_base = '6d9d3c3563027efe52b4d4ffff72314fce8e9414ae6a1e5c2d94bcc93eaf72ea40418262882ef85c80cd66b6e04aae63fe1d568777466d61de6ef4fadcf69d21'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LRE::Application.config.session_store :cookie_store, key: '_LRE_session'
