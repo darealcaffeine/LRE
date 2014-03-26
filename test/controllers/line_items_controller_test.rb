d9548f7abe230583842d4d9c76a093fe5b1c3c76
darealcaffeine <darealcaffeine@gmail.com>
2014-03-26 16:21:37 -0400

    line item error, chapter 14

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -51,10 +51,10 @@ class LineItemsControllerTest < ActionController::TestCase
     assert_difference('LineItem.count') do
       xhr :post, :create, product_id: products(:ruby).id
     end
-
     assert_response :success
     assert_select_jquery :html, '#cart' do
-      assert_select 'tr#current_item td', /Programming Ruby 1.9/
+      assert_select 'tr#current_item td'
+    end
   end
 
 end
